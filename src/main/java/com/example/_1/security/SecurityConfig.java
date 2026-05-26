66a11894444e5c7a07b8a987a5ff1ac9c41665fe
Shin <phanhoangnam17@gmail.com>
2026-05-26 15:56:06 +0700

    Update code process to test fresher Java continue

--- src/main/java/com/example/_1/security/SecurityConfig.java
+++ src/main/java/com/example/_1/security/SecurityConfig.java
@@ -46,10 +46,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("https://711-fe.vercel.app"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
